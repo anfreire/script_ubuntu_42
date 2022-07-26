623a44d2909468d38267e5a8b530c1c836c66efb
André Freire Ferreira <100360644+anfreire@users.noreply.github.com>
2022-07-26 18:41:15 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9"
+mkdir ./.themes
+cd ./.themes
+git clone https://github.com/paullinuxthemer/Mc-OS-themes/tree/master/Mc-OS-Transparent-1.3
+cd ..
+mkdir ./.icons
+cd ./.icons
+git clone https://github.com/Ok9xNirab/macOs-icon-theme
+gsettings set org.gnome.desktop.interface gtk-theme "Mc-OS-Transparent-1.3"
+gsettings set org.gnome.desktop.interface icon-theme 'MacOs-icon-theme'
+gsettings set org.gnome.desktop.wm.preferences theme "Mc-OS-Transparent-1.3"
