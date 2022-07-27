280d0847a267fdae0ce4711c66a2747e44215e6d
André Freire Ferreira <100360644+anfreire@users.noreply.github.com>
2022-07-27 18:11:09 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,15 +1,14 @@
 xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9"
+cd
 mkdir ./.themes
 cd ./.themes
-git clone https://github.com/paullinuxthemer/Mc-OS-themes.git
-mv -v ~/.themes/Mc-OS-themes/* ~/.themes
+git clone https://github.com/EliverLara/Nordic.git
 cd
 mkdir ./.icons
 cd ./.icons
 git clone https://github.com/Ok9xNirab/macOs-icon-theme
-gsettings set org.gnome.desktop.interface gtk-theme "Mc-OS-Transparent-1.3"
-gsettings set org.gnome.desktop.interface shell-theme "Mc-OS-Transparent-1.3"
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
 gsettings set org.gnome.desktop.interface icon-theme 'macOs-icon-theme'
-gsettings set org.gnome.desktop.wm.preferences theme "Mc-OS-Transparent-1.3"
-gsettings set org.gnome.shell.extensions.user-theme name "Mc-OS-Transparent-1.3"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+gsettings set org.gnome.shell.extensions.user-theme name "Nordic"
 
