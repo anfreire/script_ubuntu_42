f5051f3c7bc1a629a50eb35818a5902ea030c489
André Freire Ferreira <100360644+anfreire@users.noreply.github.com>
2022-08-29 16:23:48 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,21 +6,21 @@ echo "___  _   _           ____ _  _ ____ ____ ____ _ ____ ____ "
 echo "|__]  \_/  .         |__| |\ | |___ |__/ |___ | |__/ |___ "
 echo "|__]   |   .         |  | | \| |    |  \ |___ | |  \ |___ "
 echo ""
-xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9"
-cd script_ubuntu
-CWD=$(pwd)
-gsettings set org.gnome.desktop.background picture-uri file:///$CWD/42.png
-cd
-mkdir ./.themes
-cd ./.themes
-git clone https://github.com/EliverLara/Nordic.git
-cd
-mkdir ./.icons
-cd ./.icons
-git clone https://github.com/Ok9xNirab/macOs-icon-theme
-gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
-gsettings set org.gnome.desktop.interface icon-theme 'macOs-icon-theme'
-gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
-gsettings set org.gnome.shell.extensions.user-theme name "Nordic"
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'com.visualstudio.code.desktop']"
-xdg-open https://github.com/FreekBes/improved_intra/releases/latest/download/firefox.xpi
+xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9" 2> /dev/null
+cd script_ubuntu 2> /dev/null
+CWD=$(pwd) 2> /dev/null
+gsettings set org.gnome.desktop.background picture-uri file:///$CWD/42.png 2> /dev/null
+cd 2> /dev/null
+mkdir ./.themes 2> /dev/null
+cd ./.themes 2> /dev/null
+git clone https://github.com/EliverLara/Nordic.git 2> /dev/null
+cd 2> /dev/null
+mkdir ./.icons 2> /dev/null
+cd ./.icons 2> /dev/null
+git clone https://github.com/Ok9xNirab/macOs-icon-theme 2> /dev/null
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic" 2> /dev/null
+gsettings set org.gnome.desktop.interface icon-theme 'macOs-icon-theme' 2> /dev/null
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic" 2> /dev/null
+gsettings set org.gnome.shell.extensions.user-theme name "Nordic" 2> /dev/null
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'com.visualstudio.code.desktop']" 2> /dev/null
+xdg-open https://github.com/FreekBes/improved_intra/releases/latest/download/firefox.xpi 2> /dev/null
